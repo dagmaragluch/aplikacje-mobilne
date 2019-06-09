93c63d7bd3534086bdb3ec4f416c515c2d940ff3
dagmaragluch <Dagmara320@gmail.com>
2019-06-09 23:15:55 +0200

    add DB with Room

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -1,34 +1,53 @@
 package com.example.todo
 
+import android.arch.persistence.room.Room
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.util.Log
 import android.view.View
-import android.widget.ListView
 import android.widget.Toast
 import android.widget.AdapterView.OnItemLongClickListener
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var listView: ListView
+    private lateinit var database: AppDatabase
     val itemsArray = ArrayList<Task>()
+    lateinit var myadapter: MyArrayAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        addItems(itemsArray)
+        AsyncTask.execute {
+            try {
+                database = Room.databaseBuilder(
+                    this,
+                    AppDatabase::class.java,
+                    "student.db"
+                ).allowMainThreadQueries().build()
+            } catch (e: Exception) {
+                Log.i("am2019", e.message)
+            }
+            val t = Task("zadanie1", "2019-05-06", 1, 1)
+            database.taskDao().insertAll(t)
 
-        val myadapter = MyArrayAdapter(this, itemsArray)
+            itemsArray.addAll(database.taskDao().getAll())
+        }
+
+        myadapter = MyArrayAdapter(this, itemsArray)
         listView1.adapter = myadapter
-
 
 
         listView1.onItemLongClickListener = OnItemLongClickListener { _, _, index, _ ->
             Toast.makeText(this, "$index", Toast.LENGTH_SHORT).show()
-            itemsArray.removeAt(index)
-            myadapter.notifyDataSetChanged()
+            database.taskDao().deleteTask(itemsArray[index])
+            refrash()
             true
         }
 
@@ -36,21 +55,34 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun addTask(view: View) {
-//        val i = Intent()
-//        i.setClass(this, ItemActivity::class.java)
-//        startActivity(i)
+    fun addItem(view: View) {
+        var t = Task("zadanie2", "2019-04-16", 4, 3)
+        database.taskDao().insertTask(t)
+        refrash()
     }
 
 
-    fun addItems(tasksList: ArrayList<Task>) {
-        var task1 = Task("zadanie1", "2019-05-06", 1, 1)
-        var task2 = Task("zadanie2", "2019-04-16", 4, 3)
-        var task3 = Task("zadanie3", "2019-06-29", 2, 2)
-
-        tasksList.add(task1)
-        tasksList.add(task2)
-        tasksList.add(task3)
+    fun refrash() {
+        itemsArray.removeAll(itemsArray)
+        itemsArray.addAll(database.taskDao().getAll())
+        myadapter.notifyDataSetChanged()
     }
+
+//    fun createTask() {
+//
+//        val builder = AlertDialog.Builder(this)
+//        val inflater = layoutInflater
+//        builder.setTitle("EditText")
+//        val dialogLayout = inflater.inflate(R.layout.item_layout, null)
+//        val textView = dialogLayout.findViewById<TextView>(R.id.textTitle)
+//        builder.setView(dialogLayout)
+//        builder.setPositiveButton("OK") { dialogInterface, i ->
+//
+//            var string = textView.text.toString()
+////            mainFragment.temporaryClass.pictures[nr].description = string
+//        }
+//        builder.show()
+//
+//    }
 
 }
--- app/src/main/java/com/example/todo/Task.kt
+++ app/src/main/java/com/example/todo/Task.kt
@@ -1,5 +1,21 @@
 package com.example.todo
+//
+//import java.util.*
+//
+//data class Task (var text: String, var deadline: /*Date*/String, var image: Int, var priority: Int)
 
-import java.util.*
 
-data class Task (var text: String, var deadline: /*Date*/String, var image: Int, var priority: Int)
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "task")
+data class Task(
+    @ColumnInfo(name = "text") var text : String,
+    @ColumnInfo(name = "deadline") var deadline : String,
+    @ColumnInfo(name = "image") var image : Int,
+    @ColumnInfo(name = "priority") var priority : Int,
+    @ColumnInfo(name = "id")
+    @PrimaryKey(autoGenerate = true) var id : Long = 0
+)
